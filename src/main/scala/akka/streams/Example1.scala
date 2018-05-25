9abc7596989531e7e88702c96a37ed72c0fe2c43
ayushtiwary <er.tiwariayush@gmail.com>
2018-05-25 17:56:47 +0530

    apply Fibonacci numbers under 10M

--- src/main/scala/akka/streams/Example1.scala
+++ src/main/scala/akka/streams/Example1.scala
@@ -10,9 +10,12 @@ object Example1 extends App {
   implicit val materializer = ActorMaterializer()
   implicit val ec = system.dispatcher //ExecutionContext
 
-  val source = Source(1 to 100)
+  val source = Source.unfold(0 -> 1) {
+    case (a, _) if a > 10000000 => None
+    case (a, b) => Some((b -> (a + b)) -> a)
+  }
 
-  val done = source.runForeach(i ⇒ println(i))(materializer)
+  val done = source.runForeach(i ⇒ print(i + "   "))(materializer)
 
   done.onComplete(_ ⇒ system.terminate())
   system.whenTerminated.onComplete(_ => println("actor-system is terminated"))
